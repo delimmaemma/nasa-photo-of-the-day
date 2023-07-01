9fb88b57e3d090d800a27125260886005b1d8628
delimmaemma <calvinhtml@gmail.com>
2023-06-30 19:41:44 -0500

    Completed challenges.

---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Photo.css'
+
+const NasaPhoto = (props) => {
+    return (
+        <div className = 'nasa-photo-wrapper'>
+            <h3>{props.photo.title}</h3>
+            <p>{props.photo.date}</p>
+            <img src={props.photo.hdurl}></img>
+            <p>{props.photo.explanation}</p>
+        </div>
+    )
+}
+
+export default NasaPhoto
